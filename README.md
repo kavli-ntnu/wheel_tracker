17206dc52d32fabe38f1d24c040c7462266f8138
Horst Obenhaus <hobenhaus@gmail.com>
2018-04-13 21:35:55 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# wheel_tracker
-Code for rotary encoded wheels / tracking
+# Wheel tracker
+
+Code for rotary encoded wheels / tracking. 
+Uses a YUMO Rotary Encoder (model E6B2-CWZ3E, see [datasheet](mouser_datasheet_YUMO.pdf)) and [teensy 3.5](https://www.pjrc.com/store/teensy35.html) with [encoder library](https://www.pjrc.com/teensy/td_libs_Encoder.html). 
+
+
---
+++ encoder_arduino/encoder_arduino.ino
@@ -0,0 +1,27 @@
+/* 
+ Rotary encoder code
+ */
+
+#include <Encoder.h>
+
+// Change these two numbers to the pins connected to your encoder.
+//   Best Performance: both pins have interrupt capability
+//   Good Performance: only the first pin has interrupt capability
+//   Low Performance:  neither pin has interrupt capability
+Encoder myEnc(1,0);
+//   avoid using pins with LEDs attached
+
+void setup() {
+  Serial.begin(250000);
+  Serial.println("Basic Encoder Test:");
+}
+
+long oldPosition  = -999;
+
+void loop() {
+  long newPosition = myEnc.read();
+  if (newPosition != oldPosition) {
+    oldPosition = newPosition;
+    Serial.println(newPosition);
+  }
+}
