8dcf5851c0ac6d6ebfe5dc4e1c326fdf4f9e3574
Horst Obenhaus <hobenhaus@gmail.com>
2019-02-11 21:27:44 +0100

    Make miniscope compatible + add only rotary script (omitting motor control)

---
+++ only_rotary_teensy/only_rotary_teensy.ino
@@ -0,0 +1,103 @@
+// Teensy 3.5 control of behavioral wheel
+// This here only includes the rotary encoder
+// Default for miniscope setup
+
+#include <Encoder.h>
+
+const int led = 13;
+const int interrupter_pin = 9;
+
+const int frameclock_pin = 16;
+const int sync_pin = 15;
+
+elapsedMillis measure_timer; // measure frameclock
+
+elapsedMillis interval_timer; // for regular intervals
+unsigned int interval_ = 20;  // time in ms between serial events 
+
+bool measurement = false;
+bool start_clock = true;
+bool interrupted = false;
+bool interrupt_saved = true;
+
+long interruptedPosition;
+long newPosition;
+
+int interrupter_state;
+
+Encoder myEnc(0,1);
+
+void setup() {
+ pinMode(led, OUTPUT);
+ pinMode(sync_pin, OUTPUT);
+ 
+ pinMode(interrupter_pin, INPUT_PULLUP); 
+ pinMode(frameclock_pin, INPUT_PULLDOWN); 
+ attachInterrupt(frameclock_pin, clock_detected, RISING); // Microscope frame clock
+ attachInterrupt(interrupter_pin, interrupt_detected, RISING); // Beam interrupter
+
+ Serial.begin(250000);
+}
+
+void loop() {
+  newPosition = myEnc.read();
+
+  // ROTARY ENCODER
+  if (measurement == true){
+   
+    if (start_clock == true){ // set back timer once
+      interval_timer = interval_; 
+      start_clock = false;
+    }
+    
+    if (interval_timer >= interval_){
+      interval_timer = interval_timer-interval_;
+      digitalWriteFast(sync_pin, HIGH);
+     
+      // Serial print: Position_Interruped_InterruptedPosition_Motor
+      Serial.print(newPosition);
+      Serial.print("_");
+      if (interrupted == true){
+        Serial.print("1_");
+        Serial.print(interruptedPosition);
+        Serial.print("_");
+        interrupt_saved = true;      
+        interrupted = false;
+      } else {
+        Serial.print("0_");
+        Serial.print(interruptedPosition);
+        Serial.print("_");
+      }
+
+      Serial.println("0"); // This is reserved for motor
+         
+      delay(5);
+      
+      digitalWriteFast(sync_pin, LOW);
+      }
+    }
+
+  if(measure_timer > 500){
+    measurement = false;
+    start_clock = true;
+  }
+}
+
+void clock_detected()
+{
+  measurement = true;
+  measure_timer = 0;
+}
+
+void interrupt_detected()
+{
+  interrupted = true;
+  if(interrupt_saved == true){
+      interruptedPosition = myEnc.read(); // save current position at interrupt
+      interrupt_saved = false;            // make sure only first occurence is saved
+  }
+}
+
+
+
+
--- save_tracking.py
+++ save_tracking.py
@@ -1,11 +1,12 @@
 ## Read write encoder
 import sys,os
+from shutil import copyfile
 from datetime import datetime,timezone,timedelta
 import serial
 import csv
-from tkinter import filedialog
+import configparser
 
-port = 'COM10'
+port = 'COM7'
 max_num_values = 600
 baudrate = 250000
 
@@ -18,12 +19,12 @@ if __name__ == '__main__':
     ser = serial.Serial(port, baudrate, timeout=.5)
 
     now = datetime.now().strftime("%H-%M-%S_%m-%d-%Y")
-    root_folder = 'C:/DATA/WHEEL/'
+    root_folder = 'C:/DATA_TEMP/WHEEL/'
     output_csv = ''.join([root_folder,now,'.csv'])
     print(output_csv)
     wheel_log = open(output_csv,'a')
     wheel_log.write('Rotary encoder and motor control\n')
-    wheel_log.write('Femtonics setup\n')
+    wheel_log.write('Miniscope setup\n')
     wheel_log.write('Samping rate: 50 sps\n')
     wheel_log.write('{}\n'.format(now))
     wheel_log.write('timestamp,position,interrupt,position_at_interrupt,motor_enable\n')
@@ -43,3 +44,21 @@ if __name__ == '__main__':
             print('End of measurement.')
             wheel_log.close()
             break
+
+    # Copy the .csv file to correct filepath
+    # Open export_params.cfg and retrieve csv filepath
+    try:
+        config = configparser.ConfigParser()
+        config.read('export_params.cfg')
+        dest_csv = config['SETTINGS']['export_path']
+        sys.stdout.write('Copying file to {} '.format(dest_csv))
+        copyfile(output_csv, dest_csv)
+        sys.stdout.write('Success.')
+
+        try:
+            os.remove(output_csv)
+        except OSError:
+            pass
+    except AttributeError as err:
+        print(err)
+        print('No valid configuration found! Skipping copy step.')
