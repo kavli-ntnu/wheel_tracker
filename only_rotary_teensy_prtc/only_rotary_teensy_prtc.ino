e7c80f330c3b6ef0580d8796cae1634e1c1aa255
Horst Obenhaus <hobenhaus@gmail.com>
2023-01-13 20:25:47 +0100

    PRTC code fail saves and sanity checks

--- only_rotary_teensy_prtc/only_rotary_teensy_prtc.ino
+++ only_rotary_teensy_prtc/only_rotary_teensy_prtc.ino
@@ -1,8 +1,8 @@
 // Teensy 3.5 control of behavioral wheel
 // This here only includes the rotary encoder
 
-// This is a modified version that not only keeps track of position / interrupt data
-// but also exports the internal clock via serial 
+// This is a modified version that not only keeps track of position / interrupt data,
+// but also exports the internal clock via serial. 
 // Whenever a pseudo random timing code (TTL) is detected (PRTC), 
 // a timestamp is sent via serial 
 
@@ -10,15 +10,16 @@
 #include <Encoder.h>
 
 const int led = 13;
-const int interrupter_pin = 9;
-const int sync_pin = 15;
-
+const int interrupter_pin = 9; // Connects to the beam interrupt for full rotation measurement
+const int sync_pin = 15; // Input pin for PRTC TTLs
 
 elapsedMillis measure_timer; // internal (measurement) clock
 elapsedMillis interval_timer; // for regular intervals
+elapsedMillis interrupt_timer; // for keeping track of sync interrupts
 elapsedMillis led_on_timer; // for on time LED after sync detection
 unsigned int interval_ = 20;  // time in ms between serial events 
-unsigned int led_on_ = 20; // time the LED stays on after a sync signal is detected
+unsigned int led_on_ = 10; // time in ms the LED stays on after a sync signal is detected
+unsigned int debounce_ = 10; // debouncing time in ms for sync inputs
 
 bool measurement = false;
 bool start_clock = true;
@@ -28,12 +29,13 @@ bool interrupt_saved = true;
 long interruptedPosition;
 long newPosition;
 
-int interrupter_state;
 
 Encoder myEnc(0,1);
 
 void setup() {
  pinMode(led, OUTPUT);
+ 
+ // Define two input / interrupt routines
  pinMode(sync_pin, INPUT_PULLDOWN);
  attachInterrupt(sync_pin, sync_detected, RISING); // pseudo random timing code input
  pinMode(interrupter_pin, INPUT); // either this or attach external pullup
@@ -65,25 +67,20 @@ void loop() {
     if (interval_timer >= interval_){
       interval_timer -= interval_;
       digitalWriteFast(sync_pin, HIGH);
-      // Serial print: Position_Interruped_InterruptedPosition_Motor
       Serial.print(measure_timer);
       Serial.print("_");
       Serial.print(newPosition);
       Serial.print("_");
       if (interrupted == true){
         Serial.print("1_");
-        Serial.print(interruptedPosition);
-        Serial.print("_");
+        Serial.println(interruptedPosition);
         interrupt_saved = true;      
         interrupted = false;
       } else {
         Serial.print("0_");
-        Serial.print(interruptedPosition);
-        Serial.print("_");
-      }
+        Serial.println(interruptedPosition);
 
-      Serial.println("0"); // This is reserved for motor
-         
+      }
       delay(5); // Sync pin is HIGH for > 5 ms (serial print takes some time too)
       
       digitalWriteFast(sync_pin, LOW);
@@ -97,20 +94,27 @@ void loop() {
 }
 
 void sync_detected()
-// A PRTC TTL is detected on sync input 
-{
- Serial.println(measure_timer);
- Serial.send_now();
- led_on_timer = 0; // Reset LED on timer
- digitalWriteFast(led, HIGH);
-}
+  // A PRTC TTL is detected on sync input 
+  {
+   // Let's debounce the interrupts a bit 
+   // I am not expecting the cleanest sync pulses here and we might 
+   // overload the serial bus with writes if the pulses are too noisy...
+   if(interrupt_timer > debounce_){
+      Serial.flush();
+      Serial.println(measure_timer);
+      Serial.send_now(); // Teensy specific 
+      digitalWriteFast(led, HIGH);
+      led_on_timer = 0; // Reset LED on timer
+      interrupt_timer = 0;
+   }
+  }
 
 void interrupt_detected()
-// A full rotation of the wheel
-{
-  interrupted = true;
-  if(interrupt_saved == true){
-      interruptedPosition = myEnc.read(); // save current position at interrupt
-      interrupt_saved = false;            // make sure only first occurence is saved
+  // A full rotation of the wheel
+  {
+    interrupted = true;
+    if(interrupt_saved == true){
+        interruptedPosition = myEnc.read(); // save current position at interrupt
+        interrupt_saved = false;            // make sure only first occurence is saved
+    }
   }
-}
--- save_tracking_prtc.py
+++ save_tracking_prtc.py
@@ -2,14 +2,13 @@
 # This version works with "wheel_tracker\only_rotary_teensy_prtc/only_rotary_teensy_prtc.ino"
 # on Teensy 3.5
 # In addition to saving position / interrupt data, it also saves timestamps whenever a sync pulse is detected
+# It writes two files 
+# - Postiion data csv
+# - _sync Sync data csv
 
 
-import sys, os
-from shutil import copyfile
-from datetime import datetime, timezone, timedelta
+import datetime
 import serial
-import csv
-import configparser
 
 port = 'COM11'
 baudrate = 500000
@@ -24,7 +23,7 @@ if __name__ == '__main__':
     ser.reset_input_buffer()
     ser.reset_output_buffer()
 
-    now = datetime.now().strftime("%H-%M-%S_%m-%d-%Y")
+    now = datetime.datetime.now().strftime("%H-%M-%S_%m-%d-%Y")
     
     root_folder = 'C:/DATA_TEMP/WHEEL/'
     output_pos_csv  = ''.join([root_folder,now,'.csv'])
@@ -35,19 +34,19 @@ if __name__ == '__main__':
 
     # Open position csv and take care of header
     wheel_log = open(output_pos_csv,'a')
-    wheel_log.write('Rotary encoder and motor control POSITION DATA\n')
+    wheel_log.write('Rotary encoder POSITION DATA\n')
     wheel_log.write('Neuropixel setup\n')
     wheel_log.write('Samping rate: 50 sps\n')
     wheel_log.write('{}\n'.format(now))
-    wheel_log.write('timestamp_sys, timestamp, position, interrupt, position_at_interrupt, motor_enable\n')
+    wheel_log.write('timestamp_sys, timestamp, position, interrupt, position_at_interrupt\n')
     
     # Open sync csv and take care of header
     sync_log = open(output_sync_csv,'a')
-    sync_log.write('Rotary encoder and motor control SYNC DATA\n')
+    sync_log.write('Rotary encoder SYNC DATA\n')
     sync_log.write('Neuropixel setup\n')
     sync_log.write('Samping rate: 50 sps\n')
     sync_log.write('{}\n'.format(now))
-    sync_log.write('timestamp_sys, timestamp_sync\n')
+    sync_log.write('timestamp_sys, timestamp\n')
 
     while True:
         try:
@@ -63,15 +62,30 @@ if __name__ == '__main__':
                     except (TypeError, ValueError):
                         # Could not convert the received data to an integer timestamp
                         continue
-                    now = datetime.now().strftime('%Y-%m-%d_%H:%M:%S.%f')
+                    now = datetime.datetime.now().strftime('%Y-%m-%d_%H:%M:%S.%f')
                     sync_log.write('{},{}\n'.format(now, sync_timestamp))
 
                 else:
                     # Position / Interrupt data was received
                     wheel_info_format = wheel_info.split('_')
-                    now = datetime.now().strftime('%Y-%m-%d_%H:%M:%S.%f')
-                    print('{} | Timestamp [ms]: {} | Pos: {} | Interrupt: {} | Motor: {}'.format(now.split('.')[0], wheel_info_format[0], wheel_info_format[1], wheel_info_format[3],  wheel_info_format[4]))
-                    wheel_log.write('{},{},{},{},{}\n'.format(now,wheel_info_format[0],wheel_info_format[1],wheel_info_format[2],wheel_info_format[3], wheel_info_format[4]))
+                    if len(wheel_info_format) != 4:
+                        # This happens if an extraneous byte bleeds into the next serial write event / serial write events overlap
+                        # Just skip this iteration 
+                        continue
+                    else:
+                        try:
+                            sync_timestamp          = int(wheel_info_format[0])
+                            wheel_position          = int(wheel_info_format[1])
+                            interrupt               = wheel_info_format[2] # Interrupt 1(yes)/0(no)
+                            interrupt_position      = wheel_info_format[3] # Position at interrupt
+                        except (TypeError, ValueError):
+                            # This happens if a wrong format has been pushed over serial ... 
+                            continue
+                        
+                        now = datetime.datetime.now().strftime('%Y-%m-%d_%H:%M:%S.%f')
+                        elapsed_time_str = str(datetime.timedelta(seconds=sync_timestamp/1000.)) # Give it some nice formatting
+                        print(f'{now.split(".")[0]} | Elapsed time: {elapsed_time_str:<15} | Pos: {wheel_position:>5} | Interrupt? {interrupt}')
+                        wheel_log.write(f'{now}, {sync_timestamp}, {wheel_position}, {interrupt}, {interrupt_position}\n')
 
         except KeyboardInterrupt:
             print('End of measurement.')
